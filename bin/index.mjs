2e68fe5bc50043a051162e8bab95de1dafb04b7d
sammrosen <developmentrosen@gmail.com>
2025-10-21 19:05:30 -0700

    init

---
+++ bin/index.mjs
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log(`
+🥁  Welcome to Bongos!
+---------------------
+Use this command to create a new Bongos Base project.
+
+Example:
+  npm create bongos@latest my-app
+`);
